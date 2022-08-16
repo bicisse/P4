27216f83791e6fe07bdf7a6f53daaf54df1f875b
bicisse <105852207+bicisse@users.noreply.github.com>
2022-08-17 00:08:37 +0200

    first commit

---
+++ test.js
@@ -0,0 +1,66 @@
+//HIDE THE NAV BAR
+//________________________________________________
+const naviToggle = document.getElementById("navigation__toggle");
+// console.log(naviToggle);
+
+const showNav =document.querySelector('.navigation__nav');
+
+
+naviToggle.addEventListener('change', function () {
+   
+  
+   
+    // VISIBLE
+    if (naviToggle.checked){ 
+    console.log('checked');   
+    showNav.classList.remove('hidden');
+    }
+
+    //INVISIBLE
+    else { 
+    console.log('unchecked');  
+    showNav.classList.add('hidden');
+}})
+
+
+
+//CLOSE THE NAV BAR
+//_____________________________________________
+const closeNav = document.querySelectorAll('.closeNav');
+
+for (let i= 0; i < closeNav.length; i++)
+closeNav[i].addEventListener('click', function() {
+naviToggle.checked =false;})
+
+// SENT ALERT MESSAGE
+
+// document.getElementById('submit').addEventListener('click', function() {
+//     console.log('Message sent!');
+//     window.alert('Votre message a bien été envoyé!!')
+// } )
+
+//SCROLL BUTTON
+
+
+//Get the button:
+mybutton = document.getElementById("scroll");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+console.log('topfunction');
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+document.getElementById('scroll').addEventListener('click', topFunction);
+
